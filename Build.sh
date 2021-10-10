a8f07b8221fb841fcc284c1f9536ead8bd0b7a2c
Pierce Lewis Brooks <piercebrooks@rocketmail.com>
2021-10-10 17:07:17 -0400

    exploit new buildster features for extra distributable content

--- Build.sh
+++ Build.sh
@@ -1,4 +1,6 @@
 git submodule update --init
 python ./Buildster/buildster.py ./Project.xml
 chmod +x ./Distribution/debug/*
-patchelf --set-rpath '$ORIGIN' ./Distribution/debug/objsheet
+chmod +x ./Distribution/release/*
+patchelf --set-rpath '$ORIGIN' ./Distribution/debug/fheroes2
+patchelf --set-rpath '$ORIGIN' ./Distribution/release/fheroes2
