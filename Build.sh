b6dd0ee011406c8dbe0593e0fa54baff92d3bdbf
Pierce Brooks <piercebrooks@rocketmail.com>
2022-02-13 20:48:25 -0500

    update buildster

--- Build.sh
+++ Build.sh
@@ -1,6 +1,3 @@
 git submodule update --init
-python ./Buildster/buildster.py ./Project.xml
-chmod +x ./Distribution/debug/*
-chmod +x ./Distribution/release/*
-patchelf --set-rpath '$ORIGIN' ./Distribution/debug/fheroes2
-patchelf --set-rpath '$ORIGIN' ./Distribution/release/fheroes2
+python3 ./Buildster/buildster.py ./Project.xml
+
