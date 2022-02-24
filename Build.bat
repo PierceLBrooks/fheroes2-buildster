c5c0a91542d7270025c4b2158a57a57733ffa3de
Pierce Brooks <piercebrooks@rocketmail.com>
2022-02-23 19:01:57 -0500

    improve initial build experience

--- Build.bat
+++ Build.bat
@@ -1,2 +1,4 @@
 git submodule update --init
+python -m pip install -r ./Buildster/requirements.txt
 python ./Buildster/buildster.py ./Project.xml MINGW_TRIPLET=i686-w64-mingw32
+
--- Build.sh
+++ Build.sh
@@ -1,3 +1,4 @@
 git submodule update --init
+python3 -m pip install -r ./Buildster/requirements.txt
 python3 ./Buildster/buildster.py ./Project.xml
 
